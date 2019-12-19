ccc76713a007d4a2257d651bc4c19584bf054638
Cody Holman <holman.cody@gmail.com>
2019-12-19 17:46:19 -0600

    initial commit

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import UserForm from './components/Form.js';
+
+function App() {
+  return (
+    <div className="App">
+      <UserForm />
+    </div>
+  );
+}
+
+
+export default App;
